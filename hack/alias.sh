c1b7d8803cdd39ba6f5947394af6daa96aec1b57
Emmett Miller <emmett.miller93@gmail.com>
2022-09-11 01:34:52 +0000

    chore: add some more aliases

---
+++ hack/alias.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#
+# This script contains a collection of helpful aliases used for development.
+#
+# Usage:
+#   source ./hack/alias.sh
+#
+
+alias openapi-generator-cli='docker run openapitools/openapi-generator-cli:latest-release'
+
+alias build='yarn run build'
+alias clean='yarn run clean'
+alias lint='yarn run lint'
+alias format='yarn run format'
